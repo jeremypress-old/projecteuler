e97eb78009967923cca02555edf520fa554e6a93
jeremypress <jeremypress@berkeley.edu>
2015-07-19 21:49:30 -0700

    problem 22 done

---
+++ problem22.py
@@ -0,0 +1,17 @@
+import string
+score = dict(zip(string.ascii_lowercase, range(1, 27)))
+f = open('names.txt', 'r')
+names = f.read().split(",")
+for i in range(len(names)):
+	names[i] = names[i].strip('"').lower()
+names.sort()
+
+def problem22():
+	total = 0
+	for i in range(len(names)):
+		wordScore = 0
+		for letter in names[i]:
+			wordScore += score[letter]
+		total += wordScore * (i + 1)
+	return total
+
