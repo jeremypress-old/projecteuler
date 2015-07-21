7d58cfb6c756140162109a2d188e4310fdd93305
jeremypress <jeremypress@berkeley.edu>
2015-07-20 21:52:49 -0700

    updated problem 23

--- problem23.py
+++ problem23.py
@@ -1,16 +1,13 @@
 import EulerTools
-
-upperBound = 28123 
-possible = [False]*upperBound 
-
-abundant = []
-for i in range(12,l):
-  if EulerTools.sumDivisors(i) > i:
-    abundant.append(i)
-
-for i in range(len(abundant)):
-  for j in range(i,len(abundant)):
-    if abundant[i] + abundant[j] < upperBound:
-      possible[abundant[i] + abundant[j]] = True 
-
-print(sum([i for i in range(len(possible)) if not(possible[i])]))
+def problem23():
+	upperBound = 28123 
+	possible = [False]*upperBound 
+	abundant = []
+	for i in range(12, upperBound):
+		if EulerTools.sumDivisors(i) > i:
+			abundant.append(i)
+	for i in range(len(abundant)):
+		for j in range(i,len(abundant)):
+			if abundant[i] + abundant[j] < upperBound:
+				possible[abundant[i] + abundant[j]] = True 
+	return sum([i for i in range(len(possible)) if not(possible[i])])
